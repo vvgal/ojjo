8c7509bf165dfc0eb8c171d7c12e62c1f9520261
vvgal <vv.galchenko@mail.ru>
2022-12-11 17:53:13 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    // Tabs
+    $('.events__tabs').on('click', 'div:not(.events__tab_active, .events__substrate)', function() {
+        let now = $(this),
+            idx = $('.events__tabs > li').index(now.parent());
+        $('.events__tab').removeClass('events__tab_active');
+        $('.events__substrate').removeClass('events__substrate_active');
+        now.addClass('events__tab_active').siblings().addClass('events__substrate_active');
+        if (idx == 0) {
+            $('.events__item').addClass('events__item_active');
+        } else if (idx == 1) {
+            $('.events__item').removeClass('events__item_active');
+            $('#rings, #cufflinks, #clocks').addClass('events__item_active');
+        } else if (idx == 2) {
+            $('.events__item').removeClass('events__item_active');
+            $('#rings, #earrings, #pendants, #bracelets, #clocks').addClass('events__item_active');
+        } else if (idx == 3) {
+            $('.events__item').removeClass('events__item_active');
+            $('#cufflinks, #clocks').addClass('events__item_active');
+        } else {
+            $('.events__item').addClass('events__item_active');
+        }
+    });
+});
